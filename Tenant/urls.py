a9589601caa9a61f15db58dc32e07ea5c59e705b
NderituMwanu <mwanugerald@gmail.com>
2021-07-07 17:09:39 +0300

    app urls

--- Tenant/urls.py
+++ Tenant/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
+from tenantApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tenantApp/', include('tenantApp.urls')),
+    url(r'^$', views.homepage),
+    url('PropertyName/', views.PropertyName)
 ]
